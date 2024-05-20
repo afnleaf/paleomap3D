017b3db3803080e40f5ea76abce115bc07876184
afnleaf <afnleafow@gmail.com>
2024-05-20 13:36:48 -0400

    Added: C docker structure for building textures fast

---
+++ buildtexture/ctexture/include/common.h
@@ -0,0 +1,24 @@
+/**
+ *  @project 
+ *  @file
+ *  @author
+ *  @brief
+ */
+
+#ifndef COMMON_H
+#define COMMON_H
+// standard libs
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <limits.h>
+#include <float.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <math.h>
+
+void test();
+
+#endif
---
+++ buildtexture/ctexture/src/main.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+void test() {
+    for(int i = 0; i < 10; i++) {
+        printf("test %d\n", i);
+    }
+}
+
+int main() {
+    printf("hello world.\n");
+    test();
+    return 0;
+}
+
+
