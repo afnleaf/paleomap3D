b9fdbf5c44bf97af7f14c8886405c1bcad9b7659
afnleaf <afnleafow@gmail.com>
2024-05-23 00:18:55 -0400

    Added: Textures successful

--- buildtexture/ctexture/include/common.h
+++ buildtexture/ctexture/include/common.h
@@ -12,13 +12,17 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
+#include <endian.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <string.h>
 // installed libs
 #include <png.h>
 
-#define BUFFER_SIZE 4
 #define FILE_SIZE 6485401
-#define WIDTH 3601 //5123
-#define HEIGHT 1801 //2500
+#define NUM_FILES 109
+#define WIDTH 3600 //5123
+#define HEIGHT 1800 //2500
 
 typedef struct {
     float latitude;
@@ -37,10 +41,11 @@ void callError();
 void printBits(uint16_t num);
 Coords* generateVertices();
 size_t getBinFileSize(char* pathToFile);
-int16_t* openBinFile(char* pathToFile, size_t fileSize);
-Pixel* getPixels(Coords* coordArr, int16_t* elevationArr, size_t fileSize);
-void createImage(Pixel* pixelArr, size_t fileSize);
+int* openBinFile(char* pathToFile, size_t fileSize);
+Pixel* getPixels(Coords* coordArr, int* elevationArr, size_t fileSize);
+void createImage(Pixel* pixelArr, size_t fileSize, const char* fileName);
 void setPixel(png_bytep row, int x, int r, int g, int b);
-void getColor(int16_t elevation, int color[3]);
+void getColor(int elevation, int color[3]);
+char* getMapNum(const char* filepath);
 
 #endif
--- buildtexture/ctexture/src/main.c
+++ buildtexture/ctexture/src/main.c
@@ -4,10 +4,10 @@ void callError() {
     perror("Error reading file");
 }
 
-size_t getBinFileSize(char* pathToFile) {
+size_t getBinFileSize(char* filepath) {
     // declare vars
     FILE* fileP = NULL;
-    fileP = fopen(pathToFile, "rb");
+    fileP = fopen(filepath, "rb");
     size_t i = 0;
     if(fileP) {
         uint16_t buf;
@@ -52,29 +52,33 @@ Coords* generateVertices() {
             c++;
         }
     }
-    printf("%lu\n", c);
+    //printf("%lu\n", c);
     return coordArr;
 }
 
-int16_t* openBinFile(char* pathToFile, size_t fileSize) {
+int* openBinFile(char* filepath, size_t fileSize) {
     // declare vars
     FILE* fileP = NULL;
-    fileP = fopen(pathToFile, "rb");
-    int16_t* arr = NULL;
+    fileP = fopen(filepath, "rb");
+    int* arr = NULL;
     if(fileP) {
         // allocate memory for int16 array
-        arr = (int16_t*)malloc(sizeof(int16_t) * fileSize);
-        int16_t buffer;
+        arr = (int*)malloc(sizeof(int) * fileSize);
+        uint16_t buffer;
         // reset to top of file
         fseek(fileP, 0, SEEK_SET);
         // loop through file pointer to get file size
         int i = 0;
         while(fread(&buffer, sizeof(buffer), 1, fileP) == 1) {
             //printf("%d: %d\n", i, buffer);
-            //printBits(buf);
-            arr[i] = buffer;
+            // convert big-endian uint16_t to host endianness
+            int16_t hostBuffer = be16toh(buffer);
+            // sign-extend to int
+            arr[i] = (int)hostBuffer;
+            //arr[i] = buffer;
             i++;
         }
+        //printBits(buffer);
         // check for error
         if(ferror(fileP)) {
             callError();
@@ -86,28 +90,17 @@ int16_t* openBinFile(char* pathToFile, size_t fileSize) {
     return arr;
 }
 
- Pixel* getPixels(Coords* coordArr, int16_t* elevationArr, size_t fileSize) {
+ Pixel* getPixels(Coords* coordArr, int* elevationArr, size_t fileSize) {
     // allocate memory
     Pixel* pixelArr = (Pixel*)malloc(sizeof(Pixel) * fileSize);
     for(size_t i = 0; i < fileSize; i++) {
         float lat = coordArr[i].latitude;
         float lon = coordArr[i].longitude;
-        int16_t ele = elevationArr[i];
+        int ele = elevationArr[i];
         //printf("<%.2f> <%.2f> <%d>\n", lat, lon, ele);
-        int x = (int)((lon + 180) / 360 * WIDTH);
-        int y = (int)((90 - lat) / 180 * HEIGHT);
+        int x = (int)roundf(((lon + 180) / 360 * WIDTH));
+        int y = (int)roundf(((90 - lat) / 180 * HEIGHT));
         //printf("x<%d> y<%d>\n", x, y);
-        /*
-        // simple colour picker for now
-        int r = 255;
-        int b = 255;
-        int g = 255;
-        if(ele > 0) {
-            r = 0;
-            g = 0;
-            b = 0;
-        }
-        */
         int color[3];
         getColor(ele, color);
         int r = color[0];
@@ -122,48 +115,58 @@ int16_t* openBinFile(char* pathToFile, size_t fileSize) {
     return pixelArr;
 }
 
-void getColor(int16_t elevation, int color[3]) {
+void getColor(int elevation, int color[3]) {
     int sea_level = 0;
-    if (elevation >= -12000 && elevation < -6000) {
+    if(elevation <= -13000) {
+        color[0] = 0;  // 0x000000
+        color[1] = 255;
+        color[2] = 0;
+        printf("<%d>\n", elevation);
+    } else if(elevation > -13000 && elevation < -6000) {
         color[0] = 8;  // 0x080e30
         color[1] = 14;
         color[2] = 48;
-    } else if (elevation >= -6000 && elevation < -3000) {
+    } else if(elevation >= -6000 && elevation < -3000) {
         color[0] = 31;  // 0x1f2d47
         color[1] = 45;
         color[2] = 71;
-    } else if (elevation >= -3000 && elevation < -150) {
+    } else if(elevation >= -3000 && elevation < -150) {
         color[0] = 42;  // 0x2a3c63
         color[1] = 60;
         color[2] = 99;
-    } else if (elevation >= -150 && elevation <= sea_level) {
+    } else if(elevation >= -150 && elevation <= sea_level) {
         color[0] = 52;  // 0x344b75
         color[1] = 75;
         color[2] = 117;
-    } else if (elevation > sea_level && elevation < 100) {
+    } else if(elevation > sea_level && elevation < 100) {
         color[0] = 52;  // 0x347a2a
         color[1] = 122;
         color[2] = 42;
-    } else if (elevation >= 100 && elevation < 400) {
+    } else if(elevation >= 100 && elevation < 400) {
         color[0] = 0;  // 0x00530b
         color[1] = 53;
         color[2] = 11;
-    } else if (elevation >= 400 && elevation < 1000) {
+    } else if(elevation >= 400 && elevation < 1000) {
         color[0] = 61;  // 0x3d3704
         color[1] = 55;
         color[2] = 4;
-    } else if (elevation >= 1000 && elevation < 2000) {
+    } else if(elevation >= 1000 && elevation < 2000) {
         color[0] = 128;  // 0x805411
         color[1] = 84;
         color[2] = 68;
-    } else if (elevation >= 2000 && elevation < 3200) {
+    } else if(elevation >= 2000 && elevation < 3200) {
         color[0] = 151;  // 0x977944
         color[1] = 122;
         color[2] = 68;
-    } else {
+    } else if(elevation >= 3200) {
         color[0] = 173;  // 0xadacac
         color[1] = 172;
         color[2] = 172;
+    } else {
+        color[0] = 0;  // 0x000000
+        color[1] = 0;
+        color[2] = 0;
+        printf("<%d>\n", elevation);
     }
 }
 
@@ -174,9 +177,10 @@ void setPixel(png_bytep row, int x, int r, int g, int b) {
     px[2] = b;
 }
 
-void createImage(Pixel* pixelArr, size_t fileSize) {
+void createImage(Pixel* pixelArr, size_t fileSize, const char* fileName) {
     // open file in binary mode write
-    FILE *fp = fopen("./output/texture0.png", "wb");
+    //FILE *fp = fopen("./output/texture0.png", "wb");
+    FILE *fp = fopen(fileName, "wb");
     if(!fp) {
         perror("Failed to open file for writing.\n");
         return;
@@ -235,6 +239,7 @@ void createImage(Pixel* pixelArr, size_t fileSize) {
         int g = pixelArr[i].g;
         int b = pixelArr[i].b;
         if(x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
+            //printf("x:<%d> y:<%d>\n", x, y);
             setPixel(rowP[y], x, r, g, b);
         }
     }
@@ -250,8 +255,56 @@ void createImage(Pixel* pixelArr, size_t fileSize) {
     png_destroy_write_struct(&png, &info);
 }
 
+char** getFilepaths(const char* directory) {
+    // dirent.h
+    DIR* dir;
+    struct dirent* entry;
+    dir = opendir(directory);
+
+    // allocate memory
+    char path[256];
+    char** filepathArr = (char**)malloc(sizeof(char*) * NUM_FILES);
+    int i = 0;
+    if(dir != NULL) {
+        while((entry = readdir(dir)) != NULL) {
+            if(entry->d_type == DT_REG) {
+                strcpy(path, directory);
+                strcat(path, "/");
+                strcat(path, entry->d_name);
+                //strcat(path, "\0");
+                //printf("<%s>\n", path);
+                filepathArr[i] = (char*)malloc(sizeof(char) * strlen(path) + 1);
+                strcpy(filepathArr[i], path);
+                i++;
+            }
+        }
+        closedir(dir);
+    }
+    return filepathArr;
+}
+
+char* getMapNum(const char* filepath) {
+    char* temp = (char*)malloc(sizeof(char) * 16);
+    int i = 0;
+    while(*filepath != '\0') {
+        if(isdigit(*filepath)) {
+            //printf("<%c>\n", *filepath);
+            temp[i++] = *filepath;
+        }
+        //printf("i<%d>\n", i);
+        filepath++;
+    }
+    temp[i] = '\0';
+    //char* result = strdup(temp);
+    //free(temp);
+    return temp;
+}
+
 int main() {
     size_t fileSize = FILE_SIZE;
+    // get all filepaths
+    char** filepathArr = getFilepaths("./data_bin/large");
+    printf("File paths found.\n");
     // generate all possible coordinates
     Coords* coordArr = generateVertices();
     /*
@@ -259,28 +312,44 @@ int main() {
         printf("<%.1f> <%.1f>\n", coordArr[i].latitude, coordArr[i].longitude);
     }
     */
-    //char* pathToFile = "../../data_bin/large/Map1.bin";
-    char* pathToFile = "./data_bin/large/Map1.bin";
-    //size_t fileSize = getBinFileSize(pathToFile);
-    int16_t* elevationArr = openBinFile(pathToFile, fileSize);
-    if(elevationArr != NULL) {
-        printf("File read success.\n");
-    } else {
-        printf("File read failure.\n");
+
+    // loop through all filepaths
+    for(int i = 0; i < NUM_FILES; i++) {
+        char* filepath = filepathArr[i];
+        // get map number for output file
+        char* mapNum = getMapNum(filepath);
+        printf("<%s>\n", mapNum);
+        //const fileName = `./output/texture${mapNum}.png`;
+        char fileName[256];
+        snprintf(fileName, sizeof(fileName), "./output/texture%d.png", atoi(mapNum));
+        //size_t fileSize = getBinFileSize(filepath);
+        int* elevationArr = openBinFile(filepath, fileSize);
+        if(elevationArr != NULL) {
+            printf("File read success.\n");
+        } else {
+            printf("File read failure.\n");
+        }
+        /*
+        for(size_t i = 0; i < fileSize; i++) {
+            printf("%d: %d\n", i, elevationArr[i]);
+        }
+        */
+        Pixel* pixelArr = getPixels(coordArr, elevationArr, fileSize);
+        printf("Pixels done.\n");
+        createImage(pixelArr, fileSize, fileName);
+        printf("Image done.\n");
+        // free memory
+        free(mapNum);
+        free(elevationArr);
+        free(pixelArr);
     }
-    /*
-    for(size_t i = 0; i < fileSize; i++) {
-        printf("%d: %d\n", i, elevationArr[i]);
-    }
-    */
-    Pixel* pixelArr = getPixels(coordArr, elevationArr, fileSize);
-    printf("Pixels done.\n");
-    createImage(pixelArr, fileSize);
-    printf("Image done.\n");
+
     // free memory
+    for(int i = 0; i < NUM_FILES; i++) {
+        free(filepathArr[i]);
+    }
+    free(filepathArr);
     free(coordArr);
-    free(elevationArr);
-    free(pixelArr);
     return 0;
 }
 
